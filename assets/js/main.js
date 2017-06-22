9a2ce0f24bf95eee267ddeb9597cf4a0bc0671de
Maria Paz <maria.pazrojasc@gmail.com>
2017-06-22 04:39:34 -0400

    sección tarifa

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 18
+  });
+  var infoWindow = new google.maps.InfoWindow({map: map});
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent("Ud está aquí");
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Si el navegador no es compatible:
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
